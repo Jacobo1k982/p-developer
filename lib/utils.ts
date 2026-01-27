df6595a9454b78fe417eb5c7c7d4fc0b6f9c4551
Jacobo1k982 <askmecr@gmail.com>
2026-01-27 15:03:26 -0600

    v12

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 // lib/utils.js
-import { clsx } from "clsx"
+import { ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs) {
-    return twMerge(clsx(inputs))
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(...inputs))
 }
