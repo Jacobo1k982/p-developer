d035696896b4b56a689003750afecb956d9f1a6b
Jacobo1k982 <askmecr@gmail.com>
2026-01-28 07:58:52 -0600

    v19

--- components/Common/Nav/DrawerIntro.tsx
+++ components/Common/Nav/DrawerIntro.tsx
@@ -1,18 +1,18 @@
 'use client'
 
-import React from 'react'
+import React, { Dispatch, SetStateAction } from 'react'
 import DrawerLayout from '@/components/Common/DrawerLayout'
 import Intro from '@/components/Common/Intro/index'
 
 interface DrawerIntroProps {
-    setIsOpen: (isOpen: boolean) => void
+    setIsOpen: Dispatch<SetStateAction<boolean>>
     isOpen: boolean
 }
 
 export const DrawerIntro = ({ setIsOpen, isOpen }: DrawerIntroProps) => {
     return (
         <DrawerLayout setIsOpen={setIsOpen} isOpen={isOpen}>
-            <Intro />
+            <Intro isOpen={isOpen} setIsOpen={setIsOpen} />
         </DrawerLayout>
     )
 }
